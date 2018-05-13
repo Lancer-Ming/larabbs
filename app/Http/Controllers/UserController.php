7997a231ee1c24676502170b579a583b531bce75
Lancer-PC\Lancer <675983959@qq.com>
2018-05-14 00:22:51 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,18 +6,30 @@ use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Handlers\ImageUploadHandler;
+use Auth;
 
 class UserController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
     public function show(User $user) {
         return view('users.show', compact('user'));
     }
 
     public function edit(User $user) {
+//        $this->authorize('update', $user);
+        if(Auth::user()->cant('update', $user)) {
+            return redirect()->route('users.edit', [Auth::user()])->with('danger', '你无权编辑别人的页面~');
+        }
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user) {
+//        $this->authorize('update', $user);
+        if(Auth::user()->cant('update', $user)) {
+            return redirect()->route('users.edit', [Auth::user()])->with('danger', '你无权编辑别人的页面~');
+        }
         $data = $request->all();
 
         if ($request->avatar) {

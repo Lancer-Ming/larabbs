0a4f25a857799364f769d1a392538af6f33e69ca
Lancer-PC\Lancer <675983959@qq.com>
2018-05-26 16:54:21 +0800

    Notify author when topic replied

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,7 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'st
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only'=> ['show']]);
+
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+
+Route::resource('notifications', 'NotificationsController', ['only'=> ['index']]);

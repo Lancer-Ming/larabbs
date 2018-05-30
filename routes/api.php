65bf597eb9703113a3167b5e241716fd111b70a8
Lancer-PC\Lancer <675983959@qq.com>
2018-05-30 23:37:41 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+$api = app('Dingo\Api\Routing\Router');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api->version('v1', ['namespace' => 'App\Http\Controllers\Api'], function($api) {
+
+    // 设置的是一分钟一次
+    $api->group(['middleware' => 'api.throttle', 'limit' => config('api.rate_limits.sign.limit'), 'expires' => config('api.rate_limits.sign.expires')], function() {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')->name('api.users.store');
+    });
+
 });

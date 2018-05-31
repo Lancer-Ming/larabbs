d266689fbe5919f130995b7154ebf7676c49f735
Lancer-PC\Lancer <675983959@qq.com>
2018-05-31 22:12:19 +0800

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -9,6 +9,21 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
+        // 获取缓存的验证码信息
+        $captchaData = \Cache::get($request->captcha_key);
+
+        // 如果不存在
+        if(!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if(!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+
         $phone = $request->phone;
 
         if (!app()->environment('production')) {
--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,17 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', ['namespace' => 'App\Http\Controllers\Api'], function($api) {
 
     // 设置的是一分钟一次
-    $api->group(['middleware' => 'api.throttle', 'limit' => config('api.rate_limits.sign.limit'), 'expires' => config('api.rate_limits.sign.expires')], function() {
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
         // 用户注册
         $api->post('users', 'UsersController@store')->name('api.users.store');
+        //图形验证码
+        $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
     });
 
 });

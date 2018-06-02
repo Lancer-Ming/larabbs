cca534d4d1227237339bf123298ffc9eddefb4ca
Lancer-PC\Lancer <675983959@qq.com>
2018-06-02 19:07:37 +0800

    user update

--- routes/api.php
+++ routes/api.php
@@ -35,8 +35,12 @@ $api->version('v1', [
 
 
         $api->group(['middileware' => 'api.auth'], function($api) {
-            //当前用户登录信息
+            // 获取当前用户登录信息
             $api->get('user', 'UsersController@me')->name('api.user.show');
+            // 编辑登录用户信息
+            $api->patch('user', 'UsersController@update')->name('api.user.update');
+            // 图片资源
+            $api->post('images', 'ImagesController@store')->name('api.images.store');
         });
     });
 
